df98ed7e261afa1fb923a8567a63e3ebae39c288
Marc-Antoine Girard <62125329+marc-antoine-girard@users.noreply.github.com>
2020-10-03 10:29:48 -0400

    Spoiksss

--- Assets/Scripts/Components/Spike.cs
+++ Assets/Scripts/Components/Spike.cs
@@ -12,28 +12,41 @@ namespace Components
         public float Duration;
         public float Height = 1;
         private bool hasEnter;
+        private bool hasExit;
         private Vector3 initialPosition;
         public Collider coll;
+        public GameObject toEnable;
+        public bool loop;
+        public TriggerComponent TriggerComponent;
+        private bool hasHit;
         private void Awake()
         {
             initialPosition = Spikes.position;
+            if (loop)
+            {
+                TimedActionManager.Instance.AddTimedAction(Execute, WaitTime);
+                hasEnter = true;
+            }
+            TriggerComponent.onTriggerEnter.AddListener(() => hasHit = true);
         }
 
         public override void OnEnter(Collider other)
         {
             if (!hasEnter)
             {
-                TimedActionManager.Instance.AddTimedAction(() => StartCoroutine(Boom(other)), WaitTime);
+                TimedActionManager.Instance.AddTimedAction(Execute, WaitTime);
                 hasEnter = true;
             }
+            
         }
 
-        IEnumerator Boom(Collider other)
+        IEnumerator Boom()
         {
+            toEnable.SetActive(true);
             var time = Time.time;
             float set = 0;
             
-            while (time + Duration > Time.time)
+            while (time + Duration > Time.time && !hasHit)
             {
                 var spikePosition = initialPosition;
                 spikePosition.y = initialPosition.y + SpikeCurve.Evaluate(set / Duration) * Height;
@@ -41,6 +54,30 @@ namespace Components
                 set += Time.deltaTime;
                 yield return null;
             }
+
+            if (hasHit)
+            {
+                var spikePosition = initialPosition;
+                spikePosition.y = initialPosition.y + 1 * Height;
+                Spikes.MovePosition(spikePosition);
+            }
+
+            if (loop && !hasHit)
+            {
+                Execute();
+            }
+            else
+            {
+                hasEnter = false;
+            }
+            
+        
+        }
+
+        private void Execute()
+        {
+            StartCoroutine(Boom());
+            hasEnter = true;
         }
 
         private void OnDrawGizmos()
--- Assets/Scripts/Components/TriggerComponent.cs
+++ Assets/Scripts/Components/TriggerComponent.cs
@@ -5,28 +5,12 @@ using UnityEngine.Events;
 public class TriggerComponent : MonoBehaviour
 {
     public UnityEvent onTriggerEnter = new UnityEvent();
-    public UnityEvent onTriggerExit = new UnityEvent();
-    public UnityEvent onCollisionEnter = new UnityEvent();
-    public UnityEvent onCollisionExit = new UnityEvent();
+
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("test");
         onTriggerEnter?.Invoke();
+        PlayerManager.Instance.Killplayer();
+        GetComponent<Collider>().isTrigger = false;
     }
 
-    private void OnTriggerExit(Collider other)
-    {
-        onTriggerExit?.Invoke();
-    }
-
-    private void OnCollisionEnter(Collision other)
-    {
-        Debug.Log("test");
-        onCollisionEnter?.Invoke();
-    }
-
-    private void OnCollisionExit(Collision other)
-    {
-        onCollisionExit?.Invoke();
-    }
 }
