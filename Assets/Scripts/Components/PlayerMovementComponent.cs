f4b9e18ce0d1d9be3a773fbd661aef0bb47f38ab
Jean-Philippe Brien <46572000+Jean-Philippe-Brien@users.noreply.github.com>
2020-10-02 23:24:33 -0400

    little push

--- Assets/Scripts/Components/PlayerMovementComponent.cs
+++ Assets/Scripts/Components/PlayerMovementComponent.cs
@@ -6,13 +6,14 @@ using UnityEngine;
 public class PlayerMovementComponent : MonoBehaviour
 {
     public float Speed;
-    public Rigidbody RigidBody;
+    private Rigidbody RigidBody;
 
     private Player player;
     private int horizontalAxisId, verticalAxisId;    //TODO TO AVOID STRING COMPARISION
 
     private void Start() {
         player = ReInput.players.GetPlayer("Player01");
+        RigidBody = GetComponent<Rigidbody>();
     }
 
     private void Update() {
