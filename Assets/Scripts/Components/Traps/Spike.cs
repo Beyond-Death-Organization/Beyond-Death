12c129ec1a8e52b56cd2730f84df264e0fa49876
Gab_Desg <gabriel@khome.org>
2020-10-04 11:34:04 -0400

    Spike

---
+++ Assets/Scripts/Components/Traps/Spike.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spike : TrapComponent
+{
+    public float SpikeDelay;
+
+    private bool enabled;
+
+    private double lastOutputTime;
+    private float timer = 0;
+
+    private void Start() {
+        AnimationTimeline.stopped += director => {
+            enabled = false;
+            lastOutputTime = Time.time;
+        };
+    }
+
+    private void Update() {
+        timer += Time.deltaTime;
+        
+        if (lastOutputTime + timer >= SpikeDelay) {
+            enabled = true;
+            timer = 0;
+
+            AnimationTimeline.Play();
+        }
+    }
+
+    private void OnCollisionStay(Collision other) {
+        if (!enabled)
+            return;
+        
+        //Make sure its player
+        if (!other.gameObject.TryGetComponent(out PlayerMovementComponent player))
+            return;
+
+#if UNITY_EDITOR
+        Debug.Log("Killed played");
+#endif
+    }
+}
