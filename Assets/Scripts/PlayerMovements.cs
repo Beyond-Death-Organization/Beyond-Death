6a102210dfaaa69a2dee1feb2179916469e0c93e
Gab_Desg <gabriel@khome.org>
2020-10-12 00:22:20 -0400

    Basic player movement

--- Assets/Scripts/PlayerMovements.cs
+++ Assets/Scripts/PlayerMovements.cs
@@ -1,23 +1,29 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using Rewired;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerMovements : MonoBehaviour
 {
-    public float MovementSpeed = 10;
-    [Tooltip("")]
-    public float WorldRotation = -45;
+    public float MovementSpeed = 40;
+    [Tooltip("Camera looking at player")] public Camera Camera;
     
     protected Rewired.Player playerRef;
-    private Rigidbody rb;
 
+    private Rigidbody rb;
     private Vector3 inputs;
-    private Vector3 movements = Vector3.zero;
-    
+    private Vector3 direction = Vector3.zero;
+
+    private Transform myTransform;
+
+    private const float rotationTargetOffset = 1;
+    private Vector3 rotationTarget;
+
     protected virtual void Awake() {
+#if UNITY_EDITOR
+        if (Camera == null)
+            Debug.LogWarning("Please set a camera in player components...");
+#endif
+        myTransform = transform;
         rb = GetComponent<Rigidbody>();
     }
 
@@ -26,14 +32,44 @@ public class PlayerMovements : MonoBehaviour
     protected virtual void Update() {
         inputs.x = playerRef.GetAxisRaw("Horizontal");
         inputs.z = playerRef.GetAxisRaw("Vertical");
+
+        //UpdateTargetPosition();
     }
 
     protected virtual void FixedUpdate() {
-        //Move Horizontal / Vertical
-        movements.x = inputs.x;
-        movements.z = inputs.z;
-        movements = Quaternion.AngleAxis(WorldRotation, Vector3.up) * movements;
+        //Move depending on Camera rotation
+        direction.x = inputs.x;
+        direction.z = inputs.z;
+        direction = Quaternion.AngleAxis(Camera.transform.rotation.eulerAngles.y, Vector3.up) * direction;
 
-        rb.AddForce(movements.normalized * (MovementSpeed * Time.deltaTime), ForceMode.VelocityChange);
+        //Normalize + adjust with Time.deltaTime
+        Vector3 movementsNormalized = direction.normalized * (MovementSpeed * Time.deltaTime);
+        movementsNormalized.y = rb.velocity.y;
+
+        //Set
+        rb.velocity = movementsNormalized;
+        
+        //Rotate player toward movement direction
+        transform.rotation = Quaternion.LookRotation(transform.forward + movementsNormalized);
+    }
+
+    private void UpdateTargetPosition() {
+        //Always start from player position
+        rotationTarget = myTransform.position;
+        
+        //Update target position on X axis
+        if (inputs.x != 0)
+            rotationTarget.x += (inputs.x >= 0 ? 1 : -1 * rotationTargetOffset);
+
+
+        //Reset target position to default (default = in front of player)
+        Vector3 forward = myTransform.forward;
+        rotationTarget.x += rotationTargetOffset * forward.x;
+        rotationTarget.z += rotationTargetOffset * forward.z;
+    }
+
+    private void OnDrawGizmosSelected() {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawSphere(rotationTarget, 0.1f);
     }
 }
